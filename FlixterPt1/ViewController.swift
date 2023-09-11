2e75eb171429aa9b37e311806e55365ed060c4be
miamarte18 <miavictoria182002@gmail.com>
2023-09-11 18:03:33 -0400

    Initial Commit

---
+++ FlixterPt1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixterPt1
+//
+//  Created by mia <3 on 9/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
